3dff54d62cf7495d1284b4ace0db4d9f6afa03f8
NODA, Kai <nodakai@gmail.com>
2019-11-01 00:07:06 +0800

    Bench #0001: binomial coefficient

---
+++ CSharp/Library.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ABP.Bench
+{
+
+public interface IBench {
+    object Run(int n);
+}
+
+namespace CSharp
+{
+    public class Bench0001: IBench {
+        public object Run(int n) {
+            var ret = Run(n, n / 2);
+            return ret;
+        }
+
+        int Run(int n, int k) {
+            if (k == 0 || k == n)
+                return 1;
+            return Run(n-1, k-1) + Run(n-1, k);
+        }
+    }
+}  // namespace CSharp
+
+}  // namespace ABP.Bench
--- FSharp/Program.fs
+++ FSharp/Program.fs
@@ -1,8 +1,50 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿namespace ABP.Bench
 
-open System
+module FSharp =
+    type Bench0001() =
+        interface IBench with
+            member this.Run n =
+                let rec f n k =
+                    if k = 0 || k = n then
+                        1
+                    else
+                        f (n-1) (k-1) + f (n-1) k
+                f n (n / 2) |> box
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+module Program =
+    open System
+
+    type FsCsPair = { fs: IBench; cs: IBench }
+    type PairFactory = unit -> FsCsPair
+    let benchlist: Map<int, PairFactory> = Map.ofList [
+        1, fun () -> { fs = FSharp.Bench0001() :> IBench; cs = CSharp.Bench0001() :> IBench}
+    ]
+
+    [<EntryPoint>]
+    let main argv =
+        let mutable n = 30
+        if argv.Length < 1 then
+            failwith "choose which bench to run"
+        let ok, which = Int32.TryParse(argv.[0])
+        if not ok then
+            failwith <| sprintf "invalid bench #: '%s'" argv.[0]
+        if 1 < argv.Length then
+            let ok, n0 = Int32.TryParse(argv.[1])
+            if not ok then
+                failwith <| sprintf "invalid n: '%s'" argv.[1]
+            n <- n0
+        let factory = benchlist.[which]
+        let { fs = fs; cs = cs } = factory()
+        let run (name: string) (b: IBench) =
+            printf "running %s%d ... " name which
+            b.Run(5) |> ignore  // warmup
+            let sw = Diagnostics.Stopwatch()
+            sw.Start()
+            b.Run(n) |> printfn "%A"
+            sw.ElapsedMilliseconds
+        let tfs = run "F#" fs
+        let tcs = run "C#" cs
+        let winner = if tfs > tcs then "C#" else "F#"
+        let pct = (double tfs) / (double tcs) - 1. |> ((*) 100.)
+        printfn "%d: %d ms vs %d ms (%s is %.2f %% faster)" which tfs tcs winner pct
+        0 // return an integer exit code
