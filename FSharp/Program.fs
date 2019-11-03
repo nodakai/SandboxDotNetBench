42290e885f7813f6389f315d4b441a31646ebc1a
NODA, Kai <nodakai@gmail.com>
2019-11-03 13:48:31 +0800

    Display basic statistics

--- FSharp/Program.fs
+++ FSharp/Program.fs
@@ -14,11 +14,22 @@ module FSharp =
 module Program =
     open System
 
-    type FsCsPair = { fs: IBench; cs: IBench }
-    type PairFactory = unit -> FsCsPair
-    let benchlist: Map<int, PairFactory> = Map.ofList [
-        1, fun () -> { fs = FSharp.Bench0001() :> IBench; cs = CSharp.Bench0001() :> IBench}
+    let benchlist = Map.ofList [
+        1, fun () -> {| fs = FSharp.Bench0001() :> IBench; cs = CSharp.Bench0001() :> IBench |}
     ]
+
+    let removeOutliers arr =
+        Array.Sort(arr)
+        ArraySegment(arr, offset=1, count=arr.Length - 2)
+
+    let meanVar arr =
+        printfn "%A" arr
+        let a = Seq.average arr
+        let ss = Seq.sumBy (fun e -> (e - a) ** 2.) arr  // n * sample var
+        let n = Seq.length arr |> double
+        let v = ss / (n - 1.5)  // 1.5: prepare for an unbiased estimator of pop.stddev
+        let sd = sqrt v
+        {| mean = a; sd = sd |}
 
     [<EntryPoint>]
     let main argv =
@@ -34,17 +45,23 @@ module Program =
                 failwith <| sprintf "invalid n: '%s'" argv.[1]
             n <- n0
         let factory = benchlist.[which]
-        let { fs = fs; cs = cs } = factory()
-        let run (name: string) (b: IBench) =
-            printf "running %s%d ... " name which
+        let benchmarkers = factory()
+        let rep = 5
+        let run name (b: IBench) =
+            printf "running %s %d ... " name which
             b.Run(5) |> ignore  // warmup
-            let sw = Diagnostics.Stopwatch()
-            sw.Start()
-            b.Run(n) |> printfn "%A"
-            sw.ElapsedMilliseconds
-        let tfs = run "F#" fs
-        let tcs = run "C#" cs
-        let winner = if tfs > tcs then "C#" else "F#"
-        let pct = (double tfs) / (double tcs) - 1. |> ((*) 100.)
-        printfn "%d: %d ms vs %d ms (%s is %.2f %% faster)" which tfs tcs winner pct
+            let run _ =
+                let sw = Diagnostics.Stopwatch()
+                sw.Start()
+                let res = b.Run(n)
+                let elapsed = double sw.ElapsedMilliseconds
+                printfn "%A" res
+                elapsed
+            let raw = Array.init rep run |> removeOutliers |> meanVar
+            {| raw with name = name |}
+        let f = run "F#" benchmarkers.fs
+        let c = run "C#" benchmarkers.cs
+        let winner, loser = if f.mean > c.mean then c, f else f, c
+        let pct = loser.mean / winner.mean - 1. |> ((*) 100.)
+        printfn "%d: %s %.0f +/- %.2f ms vs %s %.0f +/- %.2f ms (%s is %.2f %% faster)" which f.name f.mean f.sd c.name c.mean c.sd winner.name pct
         0 // return an integer exit code
